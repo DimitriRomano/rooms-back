7d6259a49153a11c6b2ca70b52a89412312bfb50
MARIE Raphael <56415306+R-o-h-t@users.noreply.github.com>
2022-11-18 14:23:28 +0100

    modified userService and hotelService

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,12 +1,3 @@
-import { Injectable, Inject } from "@tsed/di";
-import { PrismaService } from "@tsed/prisma";
+import { UsersRepository } from "@tsed/prisma";
 
-@Injectable()
-export class UserService {
-  @Inject()
-  protected prisma: PrismaService;
-
-  getAll() {
-    return this.prisma.user.findMany();
-  }
-}
+export class UserService extends UsersRepository {}
